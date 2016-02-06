37a0dfaf915e0047d78c583f40d2ec57c8d61b43
Xi-Plus <huangxuanyuxiplus@gmail.com>
2016-02-06 14:16:36 +0800

    config example

---
+++ config/config_example.php
@@ -0,0 +1,14 @@
+<?php
+$config["url"] = 'http://xiplustpv.cf/';
+
+$config["hash"] = 'hash';
+
+$config['reCAPTCHA']['site_key'] = 'site_key';
+$config['reCAPTCHA']['secret_key'] = 'secret_key';
+
+$config['database']['dbname'] = 'dbname';
+$config['database']['table'] = 'table';
+
+$config['facebook']['app_id'] = 'app_id';
+$config['facebook']['app_secret'] = 'app_secret';
+?>
